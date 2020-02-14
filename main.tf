cec4474ed3455eb29f7adb53a754f9e2a268e83e
Fmarius89 <44867857+Fmarius89@users.noreply.github.com>
2020-02-14 14:43:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ module "networking" {
 module "compute" {
     source = "./compute"
     #instance_count = "${var.instance_count}"
-    #key_name = "${var.key_name}"
-    #public_key_path = "${var.public_key_path}"
+    key_name = "${var.key_name}"
+    public_key_path = "${var.public_key_path}"
     subnets = "${module.networking.public_subnets}"
     security_group = "${module.networking.public_sg}"
     subnet_ips = "${module.networking.subnet_ips}"
