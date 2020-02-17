72dd10b5125014e9ac02dd35d06161c74b7df2b4
root <root@ip-172-60-2-211.eu-west-1.compute.internal>
2020-02-17 13:03:49 +0000

    new

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "networking" {
 
 module "compute" {
     source = "./compute"
-    #instance_count = "${var.instance_count}"
+    instance_count = "${var.instance_count}"
     key_name = "${var.key_name}"
     public_key_path = "${var.public_key_path}"
     subnets = "${module.networking.public_subnets}"
